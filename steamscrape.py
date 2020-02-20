7609538601717ae90b88e41b7b1045370e46c40c
Michael <z5266084@unsw.edu.au>
2020-02-20 21:54:13 +1100

    Prints out first tab of games but then random

--- steamscrape.py
+++ steamscrape.py
@@ -1,6 +1,13 @@
+# A program that scrapes game prices from steam's main page
+
 import requests
 from bs4 import BeautifulSoup
 
+def print_new_price(name, old_price, new_price):
+    print(name.text + '   ' + old_price.text + '   ' + new_price.text)
+
+def print_old_price(name, old_price):
+    print(name.text + '   ' + old_price.text)
 
 
 if __name__ == '__main__':
@@ -9,10 +16,36 @@ if __name__ == '__main__':
     page = requests.get(url)
 
     soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Extract main page games
+    home_page_content = soup.find('div', class_ = "home_tabs_content")
+    game_tabs = home_page_content.find('div', class_ = 'tab_content')
+
+    # Each hyperlink represents a game
+    for game in game_tabs.find_all('a'):
+
+        # Extract game name and price
+        game_name = game.find('div', class_ = 'tab_item_name')
+        old_price = game.find('div', class_ = 'discount_original_price')
+        new_price = game.find('div', class_ = 'discount_final_price')
+
+        # If the game had an original price
+        if old_price is not None:
+            print_new_price(game_name, old_price, new_price)
+        else:
+            print_old_price(game_name, new_price)
+
+
+'''
     final_price =soup.find_all('div', class_ ="discount_final_price")
+
+
     original_price =soup.find_all('div', class_ ="discount_original_price")
     item_name =soup.find_all('div', class_ ="tab_item_name")
 
-    print(original_price)
-    print(final_price)
-    print(item_name)
+    print(game_tabs.prettify())
+
+    #print(original_price)
+    #print(final_price)
+    #print(item_name)
+'''
